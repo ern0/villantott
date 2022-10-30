b13bcc70164af4848824c9a33ccdd175b593bc6d
ern0 <ern0@linkbroker.hu>
2022-10-30 22:19:20 +0100

    filtering works

--- vill.py
+++ vill.py
@@ -1,8 +1,10 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
+import sys
 import datetime
 import sqlite3
 from urllib.request import urlopen
+import re
 
 DB = "vill.db"
 URL = "https://origo.hu/"
@@ -11,19 +13,45 @@ CACHE = "data/"
 
 class Vill:
 
+    PATTERNS = [
+        "melleit",
+        "meztelenül", "félmeztelenül",
+        "mutatjuk.meztelen",
+        "mutatjuk.a.meztelen",
+        "szexi", "szuperszexi",
+        "meztelenül.mutatta",
+        "meztelenül.pózolt",
+        "tökéletes.combját",
+        "tökéletes.combjait",
+        "tökéletes.alakját",
+        "tökéletes.melleit",
+    ]
+
     def main(self):
 
         conn = sqlite3.connect(DB)
         self.cursor = conn.cursor()
         self.create_table()
-        
-        self.session = str(datetime.date.today())
-        
+
+        if len(sys.argv) < 2:
+            self.session = str(datetime.date.today())
+            download = True
+        else:
+            self.session = sys.argv[1]
+            self.session = self.session.replace(CACHE, "")
+            self.session = self.session.replace(".html", "")
+            download = False
+
         self.load_page_from_cache()
-        if self.page is None:
+        if self.page is None and download:
             self.fetch_page()
             self.save_page_to_cache()
-            
+        
+        if self.page is None:
+            print("fail")
+            quit()
+
+        print(self.session + ":")
         self.proc_page()
 
     def create_table(self):
@@ -42,41 +70,69 @@ class Vill:
 
         fnam = CACHE + "/" + self.session + ".html"
         fnam = fnam.replace("//", "/")
-        
+
         return fnam
-        
+
     def load_page_from_cache(self):
-        
+
         fnam = self.get_cache_filename()
-        
+
         try:
             file = open(fnam, "r")
         except FileNotFoundError:
             self.page = None
             return
-        
+
         self.page = file.read()
         file.close()
-    
+
     def save_page_to_cache(self):
-        
+
         fnam = self.get_cache_filename()
-        
+
         file = open(fnam, "wt")
         file.write(self.page)
         file.close()
- 
+
     def fetch_page(self):
-        
+
         response = urlopen(URL)
         bytes = response.read()
         self.page = bytes.decode("utf-8")
-    
+
     def proc_page(self):
-        
+    
+        self.regexp = re.compile(r'(<!--.*?-->|<[^>]*>)')
+
         lines = self.page.split("\n")
-        for i in range(2):
-            print(lines[i])
+        for line in lines:
+
+            result = self.split_line(line)
+            if result is None:
+                continue
+            (prepared, text,) = result
+
+            for pattern in self.PATTERNS:
+                pattern = "." + pattern + "."
+                if pattern not in prepared:
+                    continue
+
+                print(" - " + text)
+                break
+
+    def split_line(self, line):
+
+        if "{{" in line:
+            return None
+
+        text = self.regexp.sub("", line).strip()
+        if text == "":
+            return None
+
+        prepared = "." + text.replace(" ", ".") + "."
+        prepared = prepared.lower()
+        
+        return (prepared, text,)
 
 if __name__ == "__main__":
     (Vill()).main()
